304862b5aa2e0b5126af3f2e21d8509caa96e6ba
maxsch3 <maxim.scherbak@gmail.com>
2020-01-24 11:46:43 +0000

    #14 bumped version in for pypi

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ your model into a one-short learning triplet model
 '''
 
 setup(name='triplet-tools',
-      version='0.1',
+      version='0.2.0',
       description='A toolbox for creating and training triplet networks in tensorflow',
       long_description=long_description,
       author='Maxim Scherbak',
